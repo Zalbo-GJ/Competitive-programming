2af718f971df6c5d8992cccf5c95f8180ad71456
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2022-10-07 21:37:39 +0300

    Time: 383 ms (80.71%), Space: 31.4 MB (57.14%) - LeetHub

---
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def kthLargestNumber(self, nums, k):
+        """
+        :type nums: List[str]
+        :type k: int
+        :rtype: str
+        """
+        nums.sort(key = lambda x: int(x))
+        
+        return nums[len(nums)-k]
+        
