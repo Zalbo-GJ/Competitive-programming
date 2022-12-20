d5bb4c409a179633f441e374f35014343e3407e1
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2022-12-20 21:08:49 +0300

    Create Average_salary.py

---
+++ Average_salary.py
@@ -0,0 +1,7 @@
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        salary.sort()
+        summ = 0
+        for i in range(1,len(salary)-1):
+            summ = summ + salary[i]
+        return summ/(len(salary)-2)
