95ab175b8c4c847176a3d9841c4b4f6110dfffb3
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-01-11 22:25:57 +0300

    Time: 51 ms (70.75%), Space: 13.9 MB (42.51%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,17 +1,11 @@
-class Solution(object):
-    def findTheWinner(self, n, k):
-        """
-        :type n: int
-        :type k: int
-        :rtype: int
-        """
-        arr = [i for i in range(1,n+1)]
-        k-=1
-        def counter(idx):
-            if len(arr)==1:
-                return arr[0]
-            idx = ((idx+k)%len(arr))
-            
-            del arr[idx]
-            return counter(idx)
-        return counter(0)
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        
+        nums = [i+1 for i in range(n)]
+        
+        ind = 0
+        while len(nums) != 1:
+            ind = (ind + k-1)% len(nums)
+            nums.remove(nums[ind])
+        
+        return nums[0]
