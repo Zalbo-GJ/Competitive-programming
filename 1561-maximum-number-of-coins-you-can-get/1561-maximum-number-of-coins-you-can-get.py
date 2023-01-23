f83f2b4201318b5cde97d245b9cefe5dd8a889ef
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-01-23 18:46:35 +0300

    Time: 676 ms (49.92%), Space: 26.4 MB (79.11%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -1,11 +1,17 @@
-class Solution(object):
-    def maxCoins(self, piles):
-        """
-        :type piles: List[int]
-        :rtype: int
-        """
-        piles.sort()
-        maxx =0
-        for i in range(len(piles)/3,len(piles),2):
-            maxx += piles[i]
-        return maxx
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        piles = sorted(piles, reverse = True)
+        
+        i = 0
+        j = 1
+        ans = 0
+        while i < len(piles)//3:
+            
+            ans += piles[j]
+            j+=2
+            i+=1
+        
+        return ans
+            
+            
+            
