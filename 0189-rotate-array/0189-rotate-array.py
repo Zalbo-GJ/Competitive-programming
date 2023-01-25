2d7bf8a52c73e233fda1ca8c30bf780b253c60e0
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-01-25 18:55:23 +0300

    Time: 380 ms (45.08%), Space: 25.5 MB (28.69%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,21 +1,33 @@
-class Solution(object):
-    def rotate(self, nums, k):
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
         """
-        :type nums: List[int]
-        :type k: int
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        self.invert(0,len(nums)-1,nums)
-        k %= len(nums)
-        self.invert(0,k-1,nums)
-        self.invert(k,len(nums)-1,nums)
-    
-    
-    def invert(self,l,r,arr):
-        
-        while l<r:
-            arr[l], arr[r] = arr[r], arr[l]
+        k%= len(nums)
+        l = 0
+        r = len(nums)-1
+        while l < r:
             
-            r-=1
-            l+=1
-        return arr
+            nums[l], nums[r] = nums[r], nums[l]
+            
+            l += 1
+            r -= 1
+            
+        l= 0
+        r = k-1
+        while l < r:
+            
+            nums[l], nums[r] = nums[r], nums[l]
+            
+            l += 1
+            r -= 1
+            
+        l= k
+        r = len(nums)-1
+        while l < r:
+            
+            nums[l], nums[r] = nums[r], nums[l]
+            
+            l += 1
+            r -= 1
+        
