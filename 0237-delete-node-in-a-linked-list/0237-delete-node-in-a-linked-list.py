a1776db6f0af44671822c9574d5618b2de2a1139
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-02-21 20:07:02 +0300

    Time: 37 ms (85.55%), Space: 14.3 MB (45.45%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -1,17 +1,15 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
+class Solution:
     def deleteNode(self, node):
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        nex = node.next
-        node.val = nex.val
-        node.next = nex.next
-        nex.next = None
-        del(nex)
+        node.val =  node.next.val
+        node.next = node.next.next
+  
