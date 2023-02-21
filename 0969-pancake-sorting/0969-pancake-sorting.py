510de3246f59c0a4d5eeafb701480765553ac98c
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-02-21 10:36:39 +0300

    Time: 52 ms (31.28%), Space: 13.8 MB (97.84%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.py
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -1,24 +1,28 @@
-class Solution(object):
-    def pancakeSort(self, arr):
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        
         """
-        :type arr: List[int]
-        :rtype: List[int]
+        1.take max element until i then flip it the flip [:i+1]
+        
         """
-        result = []
+        
         i = len(arr)-1
-        # i is current sorted border
-        while i>=0:
-            max_index = arr[0:i+1].index(max(arr[0:i+1]))
+        ans = []
+        while i >= 0:
+            maxInd = arr[:i+1].index(max(arr[:i+1]))
             
-            if max_index == i:
+            if maxInd == i:
                 pass
             else:
-                #swap to first ele
-                arr[:max_index+1] = arr[0:max_index+1][::-1]
-                result.append(max_index+1)
-                arr[:i+1] = arr[0:i+1][::-1]
-                # Swap the element to the largest pos available
-                result.append(i+1)
-            i-=1
+                arr[:maxInd+1] = arr[:maxInd+1][::-1]
+                ans.append(maxInd+1)
+                
+                arr[:i+1] = arr[:i+1][::-1]
+                ans.append(i+1)
+            i -= 1
         
-        return result
+  
+        return ans
+            
+            
+                
