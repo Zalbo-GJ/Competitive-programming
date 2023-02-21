f3a390447411266a377214e0f0b419943e9f1992
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-02-21 20:37:54 +0300

    Time: 42 ms (46.43%), Space: 14 MB (23.59%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,17 +1,34 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        if list1 and list2:
-            if list1.val > list2.val:
-                list1,list2 = list2 , list1
-            list1.next = self.mergeTwoLists(list1.next, list2)
-        return list1 or list2
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode()
+        p1 = list1
+        p2 = list2
+        curr = head
+        while p1 and p2:
+            
+            if p1.val <= p2.val:
+                curr.next = p1
+                p1 = p1.next
+            else:
+                curr.next = p2
+                p2 = p2.next
+                
+            curr = curr.next
+            
+        while p1:
+            curr.next = p1
+            p1 = p1.next
+            curr = curr.next
+        while p2:
+            curr.next = p2
+            p2 = p2.next
+            curr = curr.next
+        
+        return head.next
+            
+                
