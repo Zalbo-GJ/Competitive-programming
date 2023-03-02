fe9e65c75abac7fe9ca95fe91f159d5eca026431
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-03-02 16:08:44 +0300

    Time: 853 ms (73.17%), Space: 22.1 MB (20.94%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,21 +1,19 @@
-class Solution(object):
-    def numberOfSubarrays(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        d = {0:1}
-        pre = ans = 0
-        x = [0 if i%2==0 else 1 for i in nums]
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         
-        for i in x:
-            pre += i
+        nums = [0 if i % 2 == 0 else 1 for i in nums]
+        
+        presum = ans = 0
+        dic = {0:1}
+        for i in range(len(nums)):
+            presum += nums[i]
             
-            if pre-k in d:
-                ans += d[pre-k]
-            if pre not in d:
-                d[pre] = 1
+            if presum-k in dic:
+                ans += dic[presum-k]
+            
+            if presum in dic:
+                dic[presum] += 1
             else:
-                d[pre] += 1
+                dic[presum] = 1
+        
         return ans
