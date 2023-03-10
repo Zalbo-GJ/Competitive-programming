cba4f4fe4d157bf1a137df351b7d4557c62eb803
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-03-10 17:21:18 +0300

    Time: 28 ms (82.55%), Space: 14 MB (12.39%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,25 +1,70 @@
-class Solution(object):
-    def decodeString(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        stack = []
-        s= s[::-1]
-        i = 0
-        while i < len(s):
-            if(s[i]!="["):
-                stack.append(s[i])
-                i+=1
-            elif(s[i]=="["):
-                m = ""
-                while stack[-1]!="]":
-                    m+=stack.pop()
-                stack.pop()
-                i+=1
-                num = ""
-                while(i<len(s) and s[i].isdigit() ):
-                    num = s[i] + num
-                    i+=1
-                stack.append(m*int(num))
-        return "".join(stack[::-1])
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        ans = ""
+        
+        if s.isdigit():
+            return ans
+        def split(string):
+            res = []
+            temp = []
+            brace_count = 0
+            nested = 0
+            for i in string:
+                if i.isdigit():
+                    
+                    if temp and brace_count == 0 and not temp[-1].isdigit():
+                        res.append("".join(temp))
+                        temp = []
+                    temp.append(i)
+                elif i == "[":
+                    brace_count += 1
+                    nested += 1
+                    temp.append(i)
+                elif i == "]":
+                    brace_count -= 1
+                    temp.append(i)
+                    if brace_count == 0:
+                        res.append("".join(temp))
+                        temp = []
+                else:
+                    temp.append(i)
+            
+            if temp:
+                 res.append("".join(temp))
+                    
+                    
+            return [nested, res]
+        
+            
+        def recur(string):
+            
+            
+            nested, st = split(string)
+            
+            if nested == 0:
+                return st[0]
+            
+            dummy = []
+            for i in st:
+                if i[0].isdigit():
+                    p = 0
+                    while i[p].isdigit(): p += 1
+                    num = int(i[0:p])
+                    i = recur("".join(i[p+1:-1])) * num
+                    
+                dummy.append(i)
+            
+            return "".join(dummy)
+                    
+            
+        ans = recur(s)
+        
+        return ans        
+                
+                  
+        
+        
+        
+        
+        
