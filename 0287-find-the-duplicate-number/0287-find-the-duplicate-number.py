63fca77bd93529c745cc6430c14af9b73490f241
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-03-31 21:51:08 +0300

    Time: 736 ms (15.06%), Space: 28 MB (29.28%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -5,7 +5,9 @@ class Solution:
         
         while ind < len(nums):
             
-            if nums[nums[ind]-1] != nums[ind]:
+            if nums[ind]-1 != ind:
+                if nums[ind] == nums[nums[ind]-1]:
+                    return nums[ind]
                 nums[nums[ind]-1], nums[ind] = nums[ind], nums[nums[ind]-1]
             else:
                 ind += 1
