b6d757777744ec6127889ec24360826375ee716e
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-04-10 23:35:42 +0300

    Time: 38 ms (96.77%), Space: 16.2 MB (11.28%) - LeetHub

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        
+        depth = 0
+        def dfs(node, level):
+            nonlocal depth
+            if not node.children:
+                depth = max(depth,level)
+                return 
+            
+            level += 1
+            for child in node.children:
+                depth = max(depth,level)
+                dfs(child,level)
+            
+            
+        dfs(root,1)
+        return depth
+        
