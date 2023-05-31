200a7c268c556b749f82a91daddd4f4f1fd8949c
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-05-31 19:31:15 +0300

    Time: 1443 ms (48.85%), Space: 16.7 MB (52.61%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,23 +1,22 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = defaultdict(lambda : float('inf'))
-        def dp( summ ):
-            
-            if summ > amount:
-                return float('inf')
-            if summ == amount:
-                return 0
-            
-            if summ not in memo:
-                for coin in coins:
-                    res = dp(summ + coin) + 1
-                    memo[summ] = min(res,memo[summ])
-            
-            return memo[summ]
-    
-        ans = dp(0)
         
-        return ans if ans != float('inf') else -1
+        state = [float('inf') for _ in range(amount + 1)]
+        state[0] = 0
+        
+        for idx in range(1, amount + 1):
+            for coin in coins:
+                if idx - coin >= 0:
+                    state[idx] = min(state[idx], state[idx - coin])
+            
+            state[idx] += 1
+                    
+        return state[-1] if state[-1] != float('inf') else -1
+        
+        
+        
+        
+        
         
 
         
