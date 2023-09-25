5b1b4f510ca2e8485e551ad8b055d228a0dff9b3
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-09-25 17:39:20 +0300

    Time: 164 ms (48.54%), Space: 27.6 MB (22.47%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,18 +1,22 @@
 class Solution:
     def minSteps(self, n: int) -> int:
         
-        ans = 0
-        i = 2
+        if n == 1:
+            return 0
         
-        while i * i <= n:
-            while n % i == 0:
-                ans += i
-                n //= i
+        memo = defaultdict()
+        def dp(num, size_of_copy):
+            if num > n:
+                return float('inf')
+            if num == n:
+                return 0
+            if (num, size_of_copy) not in memo:
+                with_copy = 2 + dp(2 * num, num)
+                if num == 1:
+                    with_copy -= 1
+                without = 1 + dp(num + size_of_copy, size_of_copy)                
+                memo[(num, size_of_copy)] = min(with_copy, without)
             
-            i += 1
+            return memo[(num, size_of_copy)]
         
-        if n > 1:
-            ans += n
-            
-        
-        return ans 
+        return dp(1, 1) + 1
