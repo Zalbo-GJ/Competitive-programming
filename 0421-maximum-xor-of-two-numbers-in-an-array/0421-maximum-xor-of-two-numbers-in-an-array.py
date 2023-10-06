d8ecbb9cb4436651cdba3cd9d9e4ad638d95e5f6
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-10-06 07:11:53 +0300

    Time: 4729 ms (49.44%), Space: 307.2 MB (49.43%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.py
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def findMaximumXOR(self, nums: List[int]) -> int:
         
-        root = {}
+        root = [None, None]
         
         L = len(bin(max(nums))) - 2
         nums = [[(num >> i) & 1 for i in range(L)[::-1]] for num in nums]
@@ -11,15 +11,16 @@ class Solution:
             curr = root
             xor_node = root
             curr_xor = 0
+            
             for bit in num:
                 
-                if bit not in curr:
-                    curr[bit] = {}
+                if not curr[bit]:
+                    curr[bit] = [None, None]
                 curr = curr[bit]
                 
                 toggled_bit = 1 - bit
                 
-                if toggled_bit in xor_node:
+                if xor_node[toggled_bit]:
                     xor_node = xor_node[toggled_bit]
                     curr_xor = (curr_xor << 1) |  1
                 else:
