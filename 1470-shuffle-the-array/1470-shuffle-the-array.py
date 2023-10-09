358ba2059de128b5aa405eaf3274f3d2872ca2e7
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-10-09 22:37:46 +0300

    Time: 64 ms (43.23%), Space: 16.6 MB (19.12%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        
+        i = 0
+        j = n
+        ans = []
+        while j < len(nums):
+            ans.append(nums[i])
+            ans.append(nums[j])
+            
+            i += 1
+            j += 1
+        
+        return ans
