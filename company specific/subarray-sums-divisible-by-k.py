c3390cef876583e6609a454a4b980b51d9b8655a
Zalbowossn Gizaw <95745325+Zalbo-GJ@users.noreply.github.com>
2023-11-30 23:32:35 +0300

    Added solution for Subarray Sums Divisible by K

---
+++ company specific/subarray-sums-divisible-by-k.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        ans = 0
+        presum = 0
+        dic = {0:1}
+        
+        for i in range(len(nums)):
+            presum += nums[i]
+            if presum % k in dic:
+                ans += dic[presum % k]
+            
+            if presum % k in dic:
+                dic[presum % k] += 1
+            else:
+                dic[presum % k] = 1
+        
+        return ans
